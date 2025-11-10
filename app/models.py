28203b10eaf5c5cc1bcd94ad768581de2101de15
hardycathal <118372037+hardycathal@users.noreply.github.com>
2025-11-10 16:30:18 +0000

    Created Models And Schemas

--- app/models.py
+++ app/models.py
@@ -4,4 +4,19 @@ from sqlalchemy import String, Integer, ForeignKey, UniqueConstraint
 class Base(DeclarativeBase):
     pass
 
+class CustomerDB(Base):
+    _tablename_ = "customers"
+    id : Mapped[int] = mapped_column(primary_key=True),
+    name: Mapped[str] = mapped_column(String(100), nullable=False),
+    email: Mapped[str] = mapped_column(unique=True, nullable=False),
+    customer_since: Mapped[int] = mapped_column(Integer, nullable=False),
+
+
+class OrderDB(Base):
+    _tablename_ = "orders"
+    id : Mapped[int] = mapped_column(primary_key=True),
+    order_number: Mapped[int] = mapped_column(Integer, unique=True, nullable=False),
+    total_cents: Mapped[int] = mapped_column(Integer, nullable=False),
+    customer_id: Mapped[int] = mapped_column(ForeignKey("customers.id", ondelete="CASCADE"), nullable=False),
+    customer: Mapped[CustomerDB] = relationship(back_populates="orders"),
 
